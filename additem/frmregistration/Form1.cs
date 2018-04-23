15f4d6e9331d6f7abf62ae17de1c5c53b5f26cc0
Menahal-hassan <menahalhassan27@gmail.com>
2018-04-23 18:07:00 +0500

    Add Products and see the added or updated products

---
+++ additem/frmregistration/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmregistration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static Form1 login_frm;
+        public static Form1 getinstance()
+        {
+            if (login_frm == null)
+            {
+                login_frm = new Form1();
+            }
+            return login_frm;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdlogin_Click(object sender, EventArgs e)
+        {
+            frmlogin k3 = frmlogin.getinstance();
+            this.Hide();
+            k3.Show();
+        }
+
+        private void cmdregister_Click(object sender, EventArgs e)
+        {
+
+            Server2.Service1 myserver = new Server2.Service1();
+            if (admin.Checked)
+            {
+                if (txtpassword.Text == txtconfirmpassword.Text)
+                {
+                    myserver.Addadmin(txtusername.Text, txtpassword.Text, txtphonenumber.Text, txtaddress.Text);
+                    MessageBox.Show("Admin registered");
+                }
+                else
+                {
+                    MessageBox.Show("passsword does not match");
+                }
+
+            }
+            else if (customer.Checked)
+            {
+                if (txtpassword.Text == txtconfirmpassword.Text)
+                {
+                    myserver.Addcustomer(txtusername.Text, txtpassword.Text, txtphonenumber.Text, txtaddress.Text);
+                    MessageBox.Show("Customer registered");
+                }
+                else
+                {
+                    MessageBox.Show("passsword does not match");
+                }
+
+            }
+            else if (makeupArtist.Checked)
+            {
+                if (txtpassword.Text == txtconfirmpassword.Text)
+                {
+                    myserver.Addmakeupartist(txtusername.Text, txtpassword.Text, txtphonenumber.Text, txtaddress.Text);
+                    MessageBox.Show("Makeup Artist registered");
+                }
+                else
+                {
+                    MessageBox.Show("passsword does not match");
+                }
+
+            }
+        }
+
+        private void customer_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void makeupArtist_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtaddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtphonenumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtconfirmpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtusername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblusername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void admin_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ serviceadditem/service/CustomerDl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace service
+{
+    public class CustomerDl
+    {
+        public static List<Customer> Customers = new List<Customer>();
+        public void Addcustomer(string name ,string password ,string phone,string address)
+        {
+            Customer m = new Customer();
+            m.Custname = name;
+            m.Custpassword = password;
+            m.Custphone = phone;
+            m.Custaddress = address;
+            Customers.Add(m);
+            
+        }
+        public bool Isvalidcust(string username, string password)
+        {
+            bool iffound = false;
+            foreach (Customer c in CustomerDl.Customers)
+            {
+                if (c.Custname == username && c.Custpassword == password)
+                    iffound = true;
+            }
+            return iffound;
+
+        }
+
+    }
+}
